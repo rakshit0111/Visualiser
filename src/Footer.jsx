9ab025ecf13be2035c5881d6a849cae1d343b363
rakshit0111 <rakshitsingh333@gmail.com>
2024-09-22 23:38:24 +0530

    minor footer change

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-            <p>&copy; {currentYear} Your Name. All rights reserved.</p>
+            <p>&copy; {currentYear} Rakshit. All rights reserved.</p>
           </div>
           <div className="flex space-x-4">
             <a href="https://github.com/rakshit0111" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400 transition-colors duration-300">
